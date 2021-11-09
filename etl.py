dd28206a3ed7f3ac43147276528deda84bb7aa15
Tatiana Tikhonova <tikhonova.t@pm.me>
2021-11-09 15:18:55 -0500

    Updated process_song and _log functions

--- etl.py
+++ etl.py
@@ -2,24 +2,33 @@ import os
 import glob
 import psycopg2
 import pandas as pd
-
 from sql_queries import *
 
 
 def process_song_file(cur, filepath):
     # open song file
+    # cur: database cursor
+    # filepath: filepath to song file
     df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = list(
-        df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
-
-    cur.execute(song_table_insert, song_data)
+    for index, row in df.iterrows():
+        song_data = (row.song_id, row.title, row.artist_id,
+                     row.year, row.duration)
+        try:
+            cur.execute(song_table_insert, song_data)
+        except psycopg2.Error as error:
+            print("Error: failure to insert rows")
+            print(error)
 
     # insert artist record
-    artist_data = list(df[['artist_id', 'artist_name', 'artist_location',
-                           'artist_latitude', 'artist_longitude']].values[0])
-    cur.execute(artist_table_insert, artist_data)
+    artist_data = (row.artist_id, row.artist_name, row.artist_location,
+                   row.artist_latitude, row.artist_longitude)
+    try:
+        cur.execute(artist_table_insert, artist_data)
+    except psycopg2.Error as error:
+        print("Error: failure to insert rows")
+        print(error)
 
 
 def process_log_file(cur, filepath):
@@ -27,50 +36,63 @@ def process_log_file(cur, filepath):
     df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = df.loc[df['page'] == 'NextSong']
+    df = df[df.page == 'NextSong']
 
     # convert timestamp column to datetime
     t = pd.to_datetime(df['ts'], unit='ms')
 
     # insert time data records
-    time_data = [df.ts.values, t.dt.hour.values, t.dt.day.values,
-                 t.dt.isocalendar().week.values, t.dt.month.values, t.dt.year.values,
-                 t.dt.weekday.values]
-    column_labels = ['start_time', 'hour', 'day',
-                     'week', 'month', 'year', 'weekday']
-    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
+    timedata = (t, t.dt.hour, t.dt.day, t.dt.isocalendar().week,
+                t.dt.month, t.dt.year, t.dt.weekday)
+    columns = ('start_time', 'hour', 'day',
+                     'week', 'month', 'year', 'weekday')
+    timedf = pd.DataFrame.from_dict(dict(zip(columns, timedata)))
 
-    for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+    for i, row in timedf.iterrows():
+        try:
+            cur.execute(time_table_insert, list(row))
+        except psycopg2.Error as e:
+            print("Error: failure to insert rows")
+            print(e)
 
     # load user table
-    user_df = df[['userId', 'firstName', 'lastName',
-                  'gender', 'level']]
+    user_df = df.filter(['userId', 'firstName', 'lastName',
+                         'gender', 'level']).drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+        try:
+            cur.execute(user_table_insert, row)
+        except psycopg2.Error as e:
+            print("Error: failure to insert rows")
+            print (e)
 
     # insert songplay records
     for index, row in df.iterrows():
 
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
-        result = cur.fetchone()
-        if result:
-            songid, artistid = result
+        results = cur.fetchone()
+
+        if results:
+            songid, artistid = results
         else:
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = [row.ts, row.userId,
-                         row.level, songid, artistid, row.sessionId,
-                         row.location, row.userAgent]
+        songplay_data = [row.ts, row.userId, row.level,
+                         songid, artistid, row.sessionId, row.location,
+                         row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
+    # cur: database cursor
+    # conn: psycopg2 connection
+    # filepath: filepath to song file
+    # func: function for processing each file
+
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root, '*.json'))
